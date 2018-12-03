4403cc1ee4b33debc9732154093a1e894ff9818a
witekm1958 <45516622+witekm1958@users.noreply.github.com>
2018-12-03 22:39:46 +0100

    Create wmdim.java

---
+++ wmdim.java
@@ -0,0 +1,9 @@
+public class wmdim {
+    public static void main(String[] args) {
+        double dzielna = 10;
+        double dzielnik = 3;
+        System.out.println("Hello World :-)");
+        System.out.println("dzielenie A / B = " + (dzielna / dzielnik));
+        System.out.println("modulo A / B = " + (dzielna % dzielnik));
+    }
+}
